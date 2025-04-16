c46788f12072bbdf18ee2538b2444b8f59881fe0
Dmitrii Timofeev <28dngn@gmail.ru>
2025-04-16 15:35:29 +0300

    сделали кнопку

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,17 @@
+import { JSX } from 'react';
+import { ButtonProps } from './Button.props';
+import styles from './Button.module.css';
+import cn from 'classnames';
+
+export const Button = ({ children, appearance }: ButtonProps): JSX.Element => {
+    return (
+        <button
+            className={cn(styles.button, {
+                [styles.primary]: appearance === 'primary',
+                [styles.ghost]: appearance === 'ghost'
+            })}
+        >
+            {children}
+        </button>
+    );
+};
---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+// прокидываем отсюда, чтобы был короче путь импорта
+export * from './Htag/Htag';
+export * from './Button/Button';
